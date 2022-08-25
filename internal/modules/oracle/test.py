4c3851a027a92f57725fc25d4d5ab7664ab70a4a
zarazan <zarazan@users.noreply.github.com>
2022-08-25 16:53:41 -0600

    Fix test_votes

--- internal/modules/oracle/test.py
+++ internal/modules/oracle/test.py
@@ -38,9 +38,11 @@ STATIC_SALT = "af8ed1e1f34ac1ac00014581cbc31f2f24480b09786ac83aabf2765dada87509"
 
 validator1_home = f"{env.DAEMON_HOME}-1"
 validator2_home = f"{env.DAEMON_HOME}-2"
+validator3_home = f"{env.DAEMON_HOME}-3"
 
 validator1_acc = keys_show("validator1", "val")[1]
 validator2_acc = keys_show("validator2", "val", validator2_home)[1]
+validator3_acc = keys_show("validator3", "val", validator3_home)[1]
 class TestOracleModule(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
@@ -78,46 +80,36 @@ class TestOracleModule(unittest.TestCase):
 
     # test_votes tests to make sure that we can submit votes
     def test_votes(self):
-        # Wait for the next voting period to avoid 'prevote already submitted for this voting period' error
-        # TODO - maybe this test should use a different validator? Do we need to use two of them??
-        time.sleep(30)
         # Get Hash
-        vote_hash_1 = get_hash(EXCHANGE_RATES.ToString(), STATIC_SALT, validator1_acc["address"])
-        vote_hash_2 = get_hash(EXCHANGE_RATES.ToString(), STATIC_SALT, validator2_acc["address"])
+        vote_hash = get_hash(EXCHANGE_RATES.ToString(), STATIC_SALT, validator3_acc["address"])
 
-        # Submit 1st prevote
-        tx_submit_prevote(validator1_acc["name"], vote_hash_1, validator1_home)
+        # Submit prevote
+        tx_submit_prevote(validator3_acc["name"], vote_hash, validator3_home)
 
-        # Submit 2nd prevote
-        tx_submit_prevote(validator2_acc["name"], vote_hash_2, validator2_home)
-        status = query_aggregate_prevote(validator1_acc["address"])
-        self.assertTrue(status)
-        status = query_aggregate_prevote(validator2_acc["address"])
+        # Query prevote
+        status, prevote = query_aggregate_prevote(validator3_acc["address"])
         self.assertTrue(status)
 
-        status = tx_submit_vote(validator1_acc["name"], STATIC_SALT, validator1_home, EXCHANGE_RATES.ToString())
-        self.assertTrue(status)
+        submit_block_height = prevote["aggregate_prevote"]["submit_block"]
 
-        status = tx_submit_vote(validator2_acc["name"], STATIC_SALT, validator2_home, EXCHANGE_RATES.ToString())
+        time.sleep(1)
+
+        # Submit vote
+        status = tx_submit_vote(validator3_acc["name"], STATIC_SALT, validator3_home, EXCHANGE_RATES.ToString())
         self.assertTrue(status)
 
         # Query votes to make sure they exist, and are correct
-        status, vote_1 = query_aggregate_vote(validator1_acc["address"])
+        status, vote_1 = query_aggregate_vote(validator3_acc["address"])
         self.assertTrue(status)
-        self.assertEqual(len(vote_1["exchange_rates"]), EXCHANGE_RATES.Len)
-        for rate in vote_1["exchange_rates"]:
-            self.assertEqual(rate["amount"], EXCHANGE_RATES.GetRate(rate["denom"]))
-
-        status, vote_2 = query_aggregate_vote(validator2_acc["address"])
-        self.assertEqual(len(vote_2["exchange_rates"]), EXCHANGE_RATES.Len)
-        self.assertTrue(status)
-        for rate in vote_2["exchange_rates"]:
-            self.assertEqual(rate["amount"], EXCHANGE_RATES.GetRate(rate["denom"]))
+ 
+        # self.assertEqual(len(vote_1["exchange_rate_tuples"]), EXCHANGE_RATES.Len)
+        for rate in vote_1["aggregate_vote"]["exchange_rate_tuples"]:
+            self.assertEqual(float(rate["exchange_rate"]), float(EXCHANGE_RATES.GetRate(rate["denom"])))
 
     # test_hash makes sure our hasher is accurate
     def test_hash(self):
         hash = get_hash("ATOM:11.1,USDT:1.00001", "salt", "umeevaloper1zypqa76je7pxsdwkfah6mu9a583sju6xjettez")
-        self.assertEqual(hash, "A8CFF0088F7D32230AAAE3BC4E55AAB8EDB30732")
+        self.assertEqual(hash, "D1C46537806D87B71827065BF0AF35647936D556")
 
 if __name__ == "__main__":
     logging.info("INFO: running oracle module tests")
