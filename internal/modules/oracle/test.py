00ea7045f4481e107a2563edcef4bc52f36641da
zarazan <zarazan@users.noreply.github.com>
2022-08-28 20:28:06 -0600

    Increase reliability of test_votes test

--- internal/modules/oracle/test.py
+++ internal/modules/oracle/test.py
@@ -39,6 +39,7 @@ EXCHANGE_RATES = ExchangeRates(
     ExchangeRate("JUNO", "0.50"),
 )
 
+BLOCKS_PER_VOTING_PERIOD = 5
 STATIC_SALT = "af8ed1e1f34ac1ac00014581cbc31f2f24480b09786ac83aabf2765dada87509"
 
 validator1_home = f"{env.DAEMON_HOME}-1"
@@ -60,7 +61,8 @@ class TestOracleModule(unittest.TestCase):
         update_params = pathlib.Path().resolve().joinpath("./internal/modules/oracle/update-params.json")
         submit_and_pass_proposal(
             proposal_file_or_name=update_params,
-            proposal_type='param-change'
+            proposal_type='param-change',
+            extra_args='200uumee'
         )
 
     def test_query_oracle_params(self):
@@ -73,54 +75,64 @@ class TestOracleModule(unittest.TestCase):
         # Get Hash
         vote_hash_1 = get_hash(EXCHANGE_RATES.ToString(), STATIC_SALT, validator1_acc["address"])
         vote_hash_2 = get_hash(EXCHANGE_RATES.ToString(), STATIC_SALT, validator2_acc["address"])
+
         # Submit 1st prevote
         status = tx_submit_prevote(validator1_acc["name"], vote_hash_1, validator1_home)
         self.assertTrue(status)
+
+        # Query to verify 1st prevote exists
+        status, prevote_1 = query_aggregate_prevote(validator1_acc["address"])
+        self.assertTrue(status)
+        self.assertEqual(prevote_1["aggregate_prevote"]["hash"].upper(), vote_hash_1)
 
         # Submit 2nd prevote
         status = tx_submit_prevote(validator2_acc["name"], vote_hash_2, validator2_home)
         self.assertTrue(status)
 
-        # Query to verify prevotes exist
-        status, prevote_1 = query_aggregate_prevote(validator1_acc["address"])
-        self.assertTrue(status)
-        self.assertEqual(prevote_1["aggregate_prevote"]["hash"].upper(), vote_hash_1)
+        # Query to verify 2nd prevote exists
         status, prevote_2 = query_aggregate_prevote(validator2_acc["address"])
         self.assertTrue(status)
         self.assertEqual(prevote_2["aggregate_prevote"]["hash"].upper(), vote_hash_2)
 
+    # TODO - test to verify no aggregate pre-vote error (the vote happens more than one VP later or in same VP)
     # test_votes tests to make sure that we can submit votes
     def test_votes(self):
         # Get Hash
         vote_hash = get_hash(EXCHANGE_RATES.ToString(), STATIC_SALT, validator3_acc["address"])
 
-        # Make sure we are not at the last two blocks of a voting period
+        # If the current block height is at the end of a voting period then sleep until
+        # the beginning of the next voting period so that the pre-vote and vote land in
+        # adjacent voting periods
         block_height = int(get_block_height())
-        vp_block_height = (block_height % 5) + 1
-        if vp_block_height == 3:
-            time.sleep(2)
-        if vp_block_height == 4:
+        vp_block_height = (block_height % BLOCKS_PER_VOTING_PERIOD) + 1
+
+        # This targets the second to last and third to last blocks in a voting period.
+        # The get_block_height() function is delayed by a block by the time you read the
+        # value so it is not necessary to target the last block.
+        if vp_block_height == BLOCKS_PER_VOTING_PERIOD - 2:
+            # These timings are not exact because block time can vary depending on the hardware
             time.sleep(1)
+        if vp_block_height == BLOCKS_PER_VOTING_PERIOD - 1:
+            time.sleep(0.5)
 
         # Submit prevote
-        tx_submit_prevote(validator3_acc["name"], vote_hash, validator3_home)
-
-        # Query prevote
-        status, prevote = query_aggregate_prevote(validator3_acc["address"])
+        status, prevote_1 = tx_submit_prevote(validator3_acc["name"], vote_hash, validator3_home)
         self.assertTrue(status)
 
-        time.sleep(1)
+        # Wait until next voting period
+        time.sleep(1.25)
 
         # Submit vote
-        status = tx_submit_vote(validator3_acc["name"], STATIC_SALT, validator3_home, EXCHANGE_RATES.ToString())
+        status, vote_1 = tx_submit_vote(validator3_acc["name"], STATIC_SALT, validator3_home, EXCHANGE_RATES.ToString())
         self.assertTrue(status)
 
         # Query votes to make sure they exist, and are correct
+        # TODO - Add param to query specific blocks for test reliability
         status, vote_1 = query_aggregate_vote(validator3_acc["address"])
         self.assertTrue(status)
- 
+
         # TODO - fix this assert (only the umee price is currently returned)
-        #self.assertEqual(len(vote_1["aggregate_vote"]["exchange_rate_tuples"]), EXCHANGE_RATES.Len)
+        self.assertEqual(len(vote_1["aggregate_vote"]["exchange_rate_tuples"]), EXCHANGE_RATES.Len())
 
         for rate in vote_1["aggregate_vote"]["exchange_rate_tuples"]:
             self.assertEqual(float(rate["exchange_rate"]), float(EXCHANGE_RATES.GetRate(rate["denom"])))
