68fd9e9828a5223b0c91bfb8c875a01f2733b870
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2022-09-01 15:33:10 -0300

    add helpers

---
+++ scripts/chain/helpers/pid_control.sh
@@ -0,0 +1,24 @@
+#/bin/sh
+
+## This script serves to store Process ID functions being used in multiple files
+
+set -e
+
+file_exists() {
+  local file=$1
+  if [ -f $file ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+kill_process() {
+  local pid_file=$1
+
+  if file_exists $pid_file; then
+    pid_value=$(cat $pid_file)
+    kill -s 15 $pid_value
+    echo "-- Stopped $pid_file by killing PID: $pid_value --"
+  fi
+}
