538de2addde18ef5817f705c89546e03997f1c67
Kyle Zarazan <zarazan@users.noreply.github.com>
2022-09-02 15:19:14 -0600

    Add test for delegate-feed-consent (#14)
    Add test for delegate-feed-consent

--- internal/modules/oracle/tx.py
+++ internal/modules/oracle/tx.py
@@ -5,6 +5,7 @@ DAEMON_HOME = env.DAEMON_HOME
 RPC = env.RPC
 CHAINID = env.CHAINID
 DEFAULT_GAS = env.DEFAULT_GAS
+DEFAULT_BROADCAST_MODE = "block"
 
 # tx_submit_prevote submits an aggregate prevote tx given a hash and
 # feeder address.
@@ -12,12 +13,14 @@ def tx_submit_prevote(
     from_key,
     hash,
     home,
-    broadcast_mode='block',
+    validator="",
+    broadcast_mode=DEFAULT_BROADCAST_MODE,
     gas=DEFAULT_GAS,
 ):
-    command = f"""{DAEMON} tx oracle exchange-rate-prevote {hash} \
---chain-id {CHAINID} --keyring-backend test \
---home {home} --from {from_key} --node {RPC} --output json -y --gas {gas} -b {broadcast_mode}"""
+    command = f"""{DAEMON} tx oracle exchange-rate-prevote {hash} {validator} \
+        --chain-id {CHAINID} --keyring-backend test \
+        --home {home} --from {from_key} --node {RPC} --output json \
+        --gas {gas} -b {broadcast_mode} -y"""
     return exec_command(command)
 
 # tx_submit_vote submits an aggregate vote tx given a salt, exchange
@@ -27,10 +30,26 @@ def tx_submit_vote(
     salt,
     home,
     exchange_rates,
-    broadcast_mode='block',
+    validator="",
+    broadcast_mode=DEFAULT_BROADCAST_MODE,
     gas=DEFAULT_GAS,
 ):
-    command = f"""{DAEMON} tx oracle exchange-rate-vote {salt} {exchange_rates} \
---chain-id {CHAINID} --keyring-backend test \
---home {home} --from {from_key} --node {RPC} --output json -y --gas {gas} -b {broadcast_mode}"""
+    command = f"""{DAEMON} tx oracle exchange-rate-vote {salt} {exchange_rates} {validator} \
+        --chain-id {CHAINID} --keyring-backend test \
+        --home {home} --from {from_key} --node {RPC} --output json \
+        --gas {gas} -b {broadcast_mode} -y"""
+    return exec_command(command)
+
+# tx_delegate_feed_consent submits a tx
+def tx_delegate_feed_consent(
+    operator,
+    delegate,
+    home,
+    broadcast_mode=DEFAULT_BROADCAST_MODE,
+    gas=DEFAULT_GAS,
+):
+    command = f"""{DAEMON} tx oracle delegate-feed-consent {operator} {delegate} \
+        --chain-id {CHAINID} --keyring-backend test \
+        --home {home} --from {operator} --node {RPC} --output json \
+        --gas {gas} -b {broadcast_mode} -y"""
     return exec_command(command)
