e8916d927b48bee1ec1860c5041ae728f67ff444
ryanbajollari <54822716+rbajollari@users.noreply.github.com>
2022-09-19 07:35:45 -0400

    Leverage Liquidation Tests (#22)
    Co-authored-by: Adam Wozniak <29418299+adamewozniak@users.noreply.github.com>

--- internal/modules/leverage/query.py
+++ internal/modules/leverage/query.py
@@ -22,19 +22,19 @@ def query_registered_tokens():
 
 # query_market_summary quieries for the market summary of a specified denomination
 def query_market_summary(denom):
-    command = f"{DAEMON} q leverage market-summary ${denom} --node {RPC} \
+    command = f"{DAEMON} q leverage market-summary {denom} --node {RPC} \
 --chain-id {CHAINID} --output json"""
     return exec_command(command)
 
 # query_account_balances quieries for the total supplied, collateral, and borrowed tokens for an address
 def query_account_balances(addr):
-    command = f"{DAEMON} q leverage account-balances ${addr} --node {RPC} \
+    command = f"{DAEMON} q leverage account-balances {addr} --node {RPC} \
 --chain-id {CHAINID} --output json"""
     return exec_command(command)
 
 # query_account_summary for position USD values and borrowing limits for an address
 def query_account_summary(addr):
-    command = f"{DAEMON} q leverage account-summary ${addr} --node {RPC} \
+    command = f"{DAEMON} q leverage account-summary {addr} --node {RPC} \
 --chain-id {CHAINID} --output json"""
     return exec_command(command)
 
--- internal/modules/leverage/tx.py
+++ internal/modules/leverage/tx.py
@@ -5,18 +5,21 @@ DAEMON_HOME = env.DAEMON_HOME
 RPC = env.RPC
 CHAINID = env.CHAINID
 DEFAULT_GAS = env.DEFAULT_GAS
+DEFAULT_GAS_PRICES = env.DEFAULT_GAS_PRICES
+DEFAULT_BROADCAST_MODE = "block"
 
 # tx_supply submits a supply tx given a supplier and an amount
 def tx_supply(
     from_key,
     amount,
     home,
+    broadcast_mode=DEFAULT_BROADCAST_MODE,
     gas=DEFAULT_GAS,
+    gp=DEFAULT_GAS_PRICES,
 ):
     command = f"""{DAEMON} tx leverage supply {amount} \
 --chain-id {CHAINID} --keyring-backend test \
---home {home} --from {from_key} --node {RPC} --output json -y --gas {gas} -b block"""
-    print(command)
+--home {home} --from {from_key} --node {RPC} --output json -y --gas {gas} --gas-prices {gp} -b {broadcast_mode}"""
     return exec_command(command)
 
 # tx_withdraw submits a withdraw tx given a supplier and an amount
@@ -24,11 +27,13 @@ def tx_withdraw(
     from_key,
     amount,
     home,
+    broadcast_mode=DEFAULT_BROADCAST_MODE,
     gas=DEFAULT_GAS,
+    gp=DEFAULT_GAS_PRICES,
 ):
     command = f"""{DAEMON} tx leverage withdraw {amount} \
 --chain-id {CHAINID} --keyring-backend test \
---home {home} --from {from_key} --node {RPC} --output json -y --gas {gas} -b block"""
+--home {home} --from {from_key} --node {RPC} --output json -y --gas {gas} --gas-prices {gp} -b {broadcast_mode}"""
     return exec_command(command)
 
 # tx_collateralize submits a collateralize tx given a borrower and a coin
@@ -36,11 +41,13 @@ def tx_collateralize(
     from_key,
     coin,
     home,
+    broadcast_mode=DEFAULT_BROADCAST_MODE,
     gas=DEFAULT_GAS,
+    gp=DEFAULT_GAS_PRICES,
 ):
     command = f"""{DAEMON} tx leverage collateralize {coin} \
 --chain-id {CHAINID} --keyring-backend test \
---home {home} --from {from_key} --node {RPC} --output json -y --gas {gas} -b block"""
+--home {home} --from {from_key} --node {RPC} --output json -y --gas {gas} --gas-prices {gp} -b {broadcast_mode}"""
     return exec_command(command)
 
 # tx_decollateralize submits a decollateralize tx given a borrower and a coin
@@ -48,11 +55,13 @@ def tx_decollateralize(
     from_key,
     coin,
     home,
+    broadcast_mode=DEFAULT_BROADCAST_MODE,
     gas=DEFAULT_GAS,
+    gp=DEFAULT_GAS_PRICES,
 ):
     command = f"""{DAEMON} tx leverage decollateralize {coin} \
 --chain-id {CHAINID} --keyring-backend test \
---home {home} --from {from_key} --node {RPC} --output json -y --gas {gas} -b block"""
+--home {home} --from {from_key} --node {RPC} --output json -y --gas {gas} --gas-prices {gp} -b {broadcast_mode}"""
     return exec_command(command)
 
 # tx_borrow submits a borrow tx given a borrower and an amount
@@ -60,11 +69,13 @@ def tx_borrow(
     from_key,
     amount,
     home,
+    broadcast_mode=DEFAULT_BROADCAST_MODE,
     gas=DEFAULT_GAS,
+    gp=DEFAULT_GAS_PRICES,
 ):
     command = f"""{DAEMON} tx leverage borrow {amount} \
 --chain-id {CHAINID} --keyring-backend test \
---home {home} --from {from_key} --node {RPC} --output json -y --gas {gas} -b block"""
+--home {home} --from {from_key} --node {RPC} --output json -y --gas {gas} --gas-prices {gp} -b {broadcast_mode}"""
     return exec_command(command)
 
 # tx_repay submits a repay tx given a borrower and an amount
@@ -72,11 +83,13 @@ def tx_repay(
     from_key,
     amount,
     home,
+    broadcast_mode=DEFAULT_BROADCAST_MODE,
     gas=DEFAULT_GAS,
+    gp=DEFAULT_GAS_PRICES,
 ):
     command = f"""{DAEMON} tx leverage repay {amount} \
 --chain-id {CHAINID} --keyring-backend test \
---home {home} --from {from_key} --node {RPC} --output json -y --gas {gas} -b block"""
+--home {home} --from {from_key} --node {RPC} --output json -y --gas {gas} --gas-prices {gp} -b {broadcast_mode}"""
     return exec_command(command)
 
 # tx_liquidate submits a liquidate tx given a liquidator, borrower,
@@ -87,11 +100,13 @@ def tx_liquidate(
     amount,
     reward_denom,
     home,
+    broadcast_mode=DEFAULT_BROADCAST_MODE,
     gas=DEFAULT_GAS,
+    gp=DEFAULT_GAS_PRICES,
 ):
     command = f"""{DAEMON} tx leverage liquidate {borrower} {amount} {reward_denom} \
 --chain-id {CHAINID} --keyring-backend test \
---home {home} --from {from_key} --node {RPC} --output json -y --gas {gas} -b block"""
+--home {home} --from {from_key} --node {RPC} --output json -y --gas {gas} --gas-prices {gp} -b {broadcast_mode}"""
     return exec_command(command)
 
 # tx_update_registry submits a repay tx given a propasal_file and a deposit
@@ -100,9 +115,11 @@ def tx_update_registry(
     propasal_file,
     deposit,
     home,
+    broadcast_mode=DEFAULT_BROADCAST_MODE,
     gas=DEFAULT_GAS,
+    gp=DEFAULT_GAS_PRICES,
 ):
     command = f"""{DAEMON} tx leverage update-registry {propasal_file} {deposit} \
 --chain-id {CHAINID} --keyring-backend test \
---home {home} --from {from_key} --node {RPC} --output json -y --gas {gas} -b block"""
+--home {home} --from {from_key} --node {RPC} --output json -y --gas {gas} --gas-prices {gp} -b {broadcast_mode}"""
     return exec_command(command)
